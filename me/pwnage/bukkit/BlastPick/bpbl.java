b99e75e47af5faedb726888b4d53781361be8833
gnftoxic <toxic@pwnage.me>
2011-04-21 16:02:42 -0500

    Making /bp only effect right clicks.

--- me/pwnage/bukkit/BlastPick/bpbl.java
+++ me/pwnage/bukkit/BlastPick/bpbl.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Type;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
 
